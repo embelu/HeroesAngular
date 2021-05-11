55487ff7fad92a8f4ae47d17758d3e1191d145c3
Ludovic Baens <ludovic.baens@solidaris.be>
2021-05-11 20:47:51 +0200

    Ajout Interface hero.ts / création Hero en dur dans heroes.component.ts / Affichage du Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 
